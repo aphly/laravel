eaaa3cbf419a9cf7f16b49f5b05facccd98ff5f4
Aphly <121099327@qq.com>
2022-03-24 16:29:03 +0800

    update

--- src/Models/Dictionary.php
+++ src/Models/Dictionary.php
@@ -2,8 +2,10 @@
 
 namespace Aphly\Laravel\Models;
 
+use Aphly\Laravel\Libs\Helper;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Cache;
 
 class Dictionary extends Model
 {
@@ -14,5 +16,14 @@ class Dictionary extends Model
         'name','sort','pid','status','is_leaf','json','icon'
     ];
 
-
+    public function getMenuById($id)
+    {
+        return Cache::rememberForever('dictionary_'.$id, function () use ($id) {
+            $res['dictionary'] = self::where('status',1)->orderBy('sort', 'desc')->get()->toArray();
+            $res['dictionary_tree'] = Helper::getTree($res['dictionary'],true);
+            Helper::getTreeByid($res['dictionary_tree'],$id,$res['dictionary_tree']);
+            Helper::TreeToArr([$res['dictionary_tree']],$res['dictionary_show']);
+            return $res['dictionary_show'];
+        });
+    }
 }
