d030a88ea40f6add1cbd9cdf5bb6e5910b741fcc
Aphly <121099327@qq.com>
2022-03-28 17:36:24 +0800

    update

--- src/Models/Dictionary.php
+++ src/Models/Dictionary.php
@@ -16,14 +16,35 @@ class Dictionary extends Model
         'name','sort','pid','status','is_leaf','json','icon','value'
     ];
 
-    public function getMenuById($id)
+    public function getDictionaryById($id)
     {
         return Cache::rememberForever('dictionary_'.$id, function () use ($id) {
-            $res['dictionary'] = self::where('status',1)->orderBy('sort', 'desc')->get()->toArray();
-            $res['dictionary_tree'] = Helper::getTree($res['dictionary'],true);
-            Helper::getTreeByid($res['dictionary_tree'],$id,$res['dictionary_tree']);
+            $res['dictionary_tree'] = $this->getDictionaryTreeById($id);
             Helper::TreeToArr([$res['dictionary_tree']],$res['dictionary_show']);
             return $res['dictionary_show'];
         });
     }
+
+    public function getDictionaryTreeById($id)
+    {
+        return Cache::rememberForever('dictionary_'.$id.'_tree', function () use ($id) {
+            $res['dictionary'] = self::where('status',1)->orderBy('sort', 'desc')->get()->toArray();
+            foreach ($res['dictionary'] as $key=>$val){
+                $res['dictionary'][$key]=$val;
+                if($val['json']){
+                    $arr = json_decode($val['json'], true);
+                    $new_arr = [];
+                    foreach ($arr as $v) {
+                        $new_arr[$v['group']][] = $v;
+                    }
+                    $res['dictionary'][$key]['json'] = $new_arr;
+                }
+            }
+            $res['dictionary_tree'] = Helper::getTree($res['dictionary'],true);
+            Helper::getTreeByid($res['dictionary_tree'],$id,$res['dictionary_tree']);
+            return $res['dictionary_tree'];
+        });
+    }
+
+
 }
