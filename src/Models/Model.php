d68491775b19b3aa11ef8c6760f84b414fefb6a2
Aphly <121099327@qq.com>
2022-08-26 14:51:36 +0800

    update

--- src/Models/Model.php
+++ src/Models/Model.php
@@ -6,7 +6,12 @@ use Illuminate\Database\Eloquent\Model as ModelBase;
 
 class Model extends ModelBase
 {
+    public function fromDateTime($value){
+        return strtotime(parent::fromDateTime($value));
+    }
+
     public function findAllIds($ids) {
         return self::whereIn($this->primaryKey, $ids)->get()->keyBy($this->primaryKey)->toArray();
     }
+
 }
