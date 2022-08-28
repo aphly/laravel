362fce88ff923b3e96c26c5b3ef77f7f04b9fda1
aphly <121099327@qq.com>
2022-08-28 20:17:25 +0800

    update

--- src/Models/Model.php
+++ src/Models/Model.php
@@ -2,12 +2,18 @@
 
 namespace Aphly\Laravel\Models;
 
+use DateTimeInterface;
 use Illuminate\Database\Eloquent\Model as ModelBase;
 
 class Model extends ModelBase
 {
     public function fromDateTime($value){
         return strtotime(parent::fromDateTime($value));
+    }
+
+    protected function serializeDate(DateTimeInterface $date)
+    {
+        return $date->getTimestamp();
     }
 
     public function findAllIds($ids) {
