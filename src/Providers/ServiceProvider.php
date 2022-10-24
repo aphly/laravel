8ac3bd099e7f2df8028cee5eab8586d265e1a9e2
Aphly <121099327@qq.com>
2022-10-24 13:40:54 +0800

    update

--- src/Providers/ServiceProvider.php
+++ src/Providers/ServiceProvider.php
@@ -4,12 +4,18 @@ namespace Aphly\Laravel\Providers;
 
 class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
-    protected function addMiddlewareAlias($name, $class)
+    protected function addRouteMiddleware($name, $class)
     {
         $router = $this->app['router'];
         if (method_exists($router, 'aliasMiddleware')) {
             return $router->aliasMiddleware($name, $class);
         }
         return $router->middleware($name, $class);
+    }
+
+    protected function addMiddleware($class)
+    {
+        $router = $this->app['Illuminate\Contracts\Http\Kernel'];
+        return $router->pushMiddleware($class);
     }
 }
