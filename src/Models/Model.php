46c8b58d23fbbd04df25b49177ca8161dad3b879
aphly <121099327@qq.com>
2022-11-20 17:17:01 +0800

    update

--- src/Models/Model.php
+++ src/Models/Model.php
@@ -49,4 +49,11 @@ class Model extends ModelBase
 //        return new HasMany($instance->newQuery(), $this, $foreignKey, $localKey);
 //    }
 
+
+    public static function getEloquentSqlWithBindings($query)
+    {
+        return vsprintf(str_replace('?', '%s', $query->toSql()), collect($query->getBindings())->map(function ($binding) {
+            return is_numeric($binding) ? $binding : "'{$binding}'";
+        })->toArray());
+    }
 }
