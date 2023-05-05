f29611a68dcf652aeeb162402c34c61e44776df8
Aphly <121099327@qq.com>
2023-05-05 17:16:34 +0800

    update

--- src/Traits/Base.php
+++ src/Traits/Base.php
@@ -68,5 +68,11 @@ trait Base
         }
     }
 
+    public function laravelSql($query)
+    {
+        return vsprintf(str_replace('?', '%s', $query->toSql()), collect($query->getBindings())->map(function ($binding) {
+            return is_numeric($binding) ? $binding : "'{$binding}'";
+        })->toArray());
+    }
 
 }
