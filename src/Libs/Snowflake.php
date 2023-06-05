f2a1f1113dc73eab72adc97ec5ccd437a948af47
aphly <121099327@qq.com>
2023-06-05 19:53:49 +0800

    update

--- src/Libs/Snowflake.php
+++ src/Libs/Snowflake.php
@@ -7,10 +7,10 @@ class Snowflake
 
     static $dataCenterId = 0;      // 机房id
     static $machineId = 0;      // 机器id
-    const max12bit = 65535;
 
     public static function id($dataCenterId = 0, $machineId = 0)
     {
+        $maxbit = pow(2,17);
         self::$machineId = $machineId;
         self::$dataCenterId = $dataCenterId;
         $time = floor(microtime(true) * 1000);
@@ -18,10 +18,19 @@ class Snowflake
         $timeStr = str_pad(decbin($time), 41, "0", STR_PAD_LEFT);
         $dataCenterId = str_pad(decbin(self::$dataCenterId), 3, "0", STR_PAD_LEFT);
         $machineId = str_pad(decbin(self::$machineId), 3, "0", STR_PAD_LEFT);
-        $random = str_pad(decbin(mt_rand(0, self::max12bit)), 16, "0", STR_PAD_LEFT);
+        $random = str_pad(decbin(mt_rand(0, $maxbit)), 17, "0", STR_PAD_LEFT);
         //REDIS::incr($time.self::$dataCenterId.self::$machineId);
-        $base = $timeStr . $dataCenterId . $machineId . $random;
-        return bindec($base);
+        return bindec($timeStr . $dataCenterId . $machineId . $random);
+    }
+
+    public static function orderId()
+    {
+        $maxbit = pow(2,23);
+        $time = floor(microtime(true) * 1000);
+        $time -= self::EPOCH;
+        $timeStr = str_pad(decbin($time), 41, "0", STR_PAD_LEFT);
+        $random = str_pad(decbin(mt_rand(0, $maxbit)), 23, "0", STR_PAD_LEFT);
+        return bindec($timeStr . $random);
     }
 }
 
