f11d13c837e7b719ee98a04f78d0b85b4b454dfd
Aphly <121099327@qq.com>
2023-06-06 14:04:41 +0800

    update

--- src/Libs/Snowflake.php
+++ src/Libs/Snowflake.php
@@ -1,12 +1,16 @@
 <?php
 namespace Aphly\Laravel\Libs;
 
+use Illuminate\Support\Facades\Redis;
+
 class Snowflake
 {
     const EPOCH = 1640995200000;
 
     static $dataCenterId = 0;      // 机房id
     static $machineId = 0;      // 机器id
+
+    static $redis = false;
 
     public static function id($dataCenterId = 0, $machineId = 0)
     {
@@ -18,19 +22,43 @@ class Snowflake
         $timeStr = str_pad(decbin($time), 41, "0", STR_PAD_LEFT);
         $dataCenterId = str_pad(decbin(self::$dataCenterId), 3, "0", STR_PAD_LEFT);
         $machineId = str_pad(decbin(self::$machineId), 3, "0", STR_PAD_LEFT);
-        $random = str_pad(decbin(mt_rand(0, $maxbit)), 17, "0", STR_PAD_LEFT);
-        //REDIS::incr($time.self::$dataCenterId.self::$machineId);
+        if(self::$redis){
+            $random = str_pad(decbin(self::redisId(self::$dataCenterId.self::$machineId,$maxbit)), 23, "0", STR_PAD_LEFT);
+        }else{
+            $random = str_pad(decbin(mt_rand(0, $maxbit)), 17, "0", STR_PAD_LEFT);
+        }
         return bindec($timeStr . $dataCenterId . $machineId . $random);
     }
 
-    public static function orderId()
+    public static function incrId($key='order')
     {
         $maxbit = pow(2,23);
         $time = floor(microtime(true) * 1000);
         $time -= self::EPOCH;
         $timeStr = str_pad(decbin($time), 41, "0", STR_PAD_LEFT);
-        $random = str_pad(decbin(mt_rand(0, $maxbit)), 23, "0", STR_PAD_LEFT);
+        if(self::$redis){
+            $random = str_pad(decbin(self::redisId($key,$maxbit)), 23, "0", STR_PAD_LEFT);
+        }else{
+            $random = str_pad(decbin(mt_rand(0, $maxbit)), 23, "0", STR_PAD_LEFT);
+        }
         return bindec($timeStr . $random);
+    }
+
+    public static function redisId($key,$maxbit)
+    {
+        $key = 'incr:'.$key.':'.date("Y:m:d");
+        $values = Redis::get($key);
+        if($values){
+            if($values>=$maxbit){
+                $values = 1;
+                Redis::set($key,$values);
+            }else{
+                $values = Redis::incr($key);
+            }
+        }else{
+            $values = Redis::incr($key);
+        }
+        return $values;
     }
 }
 
